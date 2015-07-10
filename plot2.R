932739cc50f06fd2a45b19110c5204df2832d0f5
Kuan Hoong <kuanhoong@gmail.com>
2015-07-10 10:15:13 +0800

    plot 3

--- plot2.R
+++ plot2.R
@@ -4,22 +4,20 @@ setwd("C:/Users/Kuan/Desktop/exdata/")
 library(data.table)
 
 ## Getting full dataset
-DT1 <- read.csv("./household_power_consumption.txt", header=TRUE, sep=';', na.strings="?",nrows=2075259, check.names=FALSE, stringsAsFactors=FALSE, comment.char="", quote='\"')
+DT<-fread("./household_power_consumption.txt", na.strings="?")
 
-DT1<-fread("./household_power_consumption.txt", na.strings="?")
-
-DT1$Date <- as.Date(DT1$Date, format="%d/%m/%Y")
+DT$Date <- as.Date(DT1$Date, format="%d/%m/%Y")
 
 ## Subsetting the data
-DT1.filter <- subset(DT1, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+DT.filter <- subset(DT1, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
 rm(DT1)
 
 ## Converting dates
-datetime <- paste(as.Date(DT1.filter$Date), DT1.filter$Time)
-DT1.filter$Datetime <- as.POSIXct(datetime)
+datetime <- paste(as.Date(DT.filter$Date), DT.filter$Time)
+DT.filter$Datetime <- as.POSIXct(datetime)
 
 #output histogram to PNG
-plot(DT1.filter$Global_active_power~DT1.filter$Datetime, type="l",
+plot(DT.filter$Global_active_power~DT.filter$Datetime, type="l",
      ylab="Global Active Power (kilowatts)", xlab="")
 dev.copy(png, file="plot2.png", height=480, width=480)
 
