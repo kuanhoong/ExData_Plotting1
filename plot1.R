a6a106c210e452c4ca7cf13caac94970d38471ee
Kuan Hoong <kuanhoong@gmail.com>
2015-07-10 09:52:23 +0800

    plot 2

--- plot1.R
+++ plot1.R
@@ -12,6 +12,7 @@ DT$strDate <- as.Date(DT$Date,"%d/%m/%Y")
 #select data 
 DT.filter<-DT[DT$strDate=="2007-02-01" | DT$strDate=="2007-02-02",]
 DT.filter$Global_active_power<-as.numeric(DT.filter$Global_active_power)
+rm(DT)
 
 #output histogram to PNG
 png(filename="plot1.png", width=480, height=480)
