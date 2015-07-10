0377d3e478eead20a4ecc94f0cfa51ed5b0f9e0a
Kuan Hoong <kuanhoong@gmail.com>
2015-07-10 10:35:05 +0800

    plot 4

--- plot4.R
+++ plot4.R
@@ -22,15 +22,14 @@ with(DT.filter, {
   plot(Global_active_power~Datetime, type="l",
        ylab="Global Active Power", xlab="")
   plot(Voltage~Datetime, type="l",
-       ylab="Voltage (volt)", xlab="")
+       ylab="Voltage (volt)", xlab="datetime")
   plot(Sub_metering_1~Datetime, type="l",
        ylab="Energy sub metering", xlab="")
-  lines(Sub_metering_2~Datetime,col='Red')
-  lines(Sub_metering_3~Datetime,col='Blue')
-  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
-         legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex = 0.75)
+       lines(Sub_metering_2~Datetime,col='Red')
+       lines(Sub_metering_3~Datetime,col='Blue')
+       legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex = 0.75)
   plot(Global_reactive_power~Datetime, type="l",
-       ylab="Global Rective Power",xlab="")
+       ylab="Global_Rective_Power",xlab="datetime")
 })
 ## Saving to file
 dev.copy(png, file="plot4.png", height=480, width=480)
